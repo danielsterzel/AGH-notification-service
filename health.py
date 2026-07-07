0139bbfac7e0af315c7ceb4aeff4af0d48a7356a
daniel <daniel.j.sterzel@gmaik.com>
2026-07-07 08:53:46 +0200

    health endpoint

---
+++ health.py
@@ -0,0 +1,13 @@
+
+from aiohttp import web
+
+async def health(request):
+    return web.Response(text="ok")
+
+async def start_health_server():
+    app = web.Application()
+    app.router.add_get("/health", health)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", 8080)
+    await site.start()
